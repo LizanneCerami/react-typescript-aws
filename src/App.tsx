4947d5f8ace5910db229c0a59cf912f3051021bc
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-05-04 15:56:32 -0400

    added dogs

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,23 @@ import { Form } from './components/Form'
 import './App.css';
 
 function App() {
+
+  const [ serverData, setServerData ] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('http://54.152.255.167:4000/')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <Hello firstName='Liz' lastName='Cerami'/>
         <Form />
+        {/*@ts-ignore */}
+        {serverData && serverData.map(eachDog => <p>{eachDog.name} - {eachDog}</p>)}
       </header>
     </div>
   );
